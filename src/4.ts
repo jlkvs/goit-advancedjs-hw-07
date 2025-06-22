aa0d26e76df188499bf2227646dc66a3974031a1
julia <juljuliakv@gmail.com>
2025-06-23 01:46:21 +0200

    Homework

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,68 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected door: boolean = false; 
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log(`Person entered. Tenants count: ${this.tenants.length}`);
+      } else {
+        console.log("Door is closed. Can't come in.");
+      }
+    }
+  
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log("Door is opened!");
+      } else {
+        console.log("Wrong key! Door remains closed.");
+      }
+    }
+  }
+  
+ 
+  
+  const key = new Key();
+  
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey()); 
+  house.comeIn(person);            
+  
+  export {};
+  
